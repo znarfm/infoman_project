b24ee7aa9ed4d780afbbf71dac1f93a7bcc9e8a2
znarfm <44516102+znarfm@users.noreply.github.com>
2024-07-01 00:31:08 +0800

    add about page

---
+++ pages/about.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="About project",
+    page_icon="🧑‍💻",
+    layout="wide",
+)
+
+st.logo(image="./images/NCSC.png")
+st.page_link("main.py", label="Back", icon="🔙")
+st.header("National Commission of Senior Citizens", divider="rainbow", anchor=False)
+
+st.markdown("### Members")
+c1, c2, c3, c4 = st.columns(4)
+with c1:
+    st.image("./images/placeholder.png")
+    st.markdown("[Meinard Adrian Francisco](https://github.com/znarfm)")
+with c2:
+    st.image("./images/placeholder.png")
+    st.write("[Prince Bryll Soliven](https://www.linkedin.com/in/prince-bryll-soliven/)")
+with c3:
+    st.image("./images/placeholder.png")
+    st.write("[Jedric Knight Vicente](https://github.com/HalluciKnight)")
+with c4:
+    st.image("./images/placeholder.png")
+    st.write("[Novelle Estrella](https://github.com/Novelle-Estrella)")
+
+st.divider()
+st.markdown("### Links")
+st.page_link(page="https://github.com/znarfm/infoman_project", label="GitHub Repository", icon="👨‍💻")
+st.page_link(page="https://www.ncsc.gov.ph/", label="Official NCSC Website", icon="🌐")
+
+st.warning("This website is an independent project and is not affiliated with [NCSC](https://www.ncsc.gov.ph/). It is intended solely for academic purposes.", icon="⚠️")
